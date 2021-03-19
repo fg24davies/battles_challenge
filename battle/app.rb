755bb76e90e4d76cfcffd9cfbaff12fb6485bb31
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-03-19 13:12:08 +0000

    Refactored game instances to accept two player instances as arguments
    Co-authored-by: Flora Davies <fg24davies@users.noreply.github.com>

--- battle/app.rb
+++ battle/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative './lib/player.rb'
+require_relative './lib/game.rb'
 
 
 class Battle < Sinatra::Base
@@ -11,26 +12,20 @@ class Battle < Sinatra::Base
   end
   
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
   
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_1_hit_points = $player_1.hitpoints
-    @player_2_hit_points = $player_2.hitpoints
+    @game = $game
     erb :play
   end 
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    game = Game.new
-    game.attack($player_2)
-    @player_1_hit_points = $player_1.hitpoints
-    @player_2_hit_points = $player_2.hitpoints
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end 
 end 
--- battle/spec/game_spec.rb
+++ battle/spec/game_spec.rb
@@ -4,7 +4,19 @@ describe Game do
   
   let(:player_1) { double :player }
   let(:player_2) { double :player }
-  let(:game) { Game.new }
+  let(:game) { Game.new(player_1, player_2) }
+
+  describe '#player_1' do
+    it 'retrieves player 1 object' do
+      expect(game.player_1).to eq(player_1)
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves player 2 object' do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
 
   describe '#attack' do 
     it 'reduces HP by 10 after an attack' do
